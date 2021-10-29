5df50f7292b2094c82d3330304424f9c2c630339
vuhai2310 <lvhai.19it4@vku.udn.vn>
2021-10-29 16:30:21 +0700

    2 - Update 3D Card

--- js/card3D.js
+++ js/card3D.js
@@ -13,15 +13,15 @@ const btn = $(".card-body .btn");
 
 //Moving animation event
 card.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 6;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 15;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 
 //animate in
 card.addEventListener("mouseenter", (e) => {
   card.style.transition = `all 0.2s ease`;
-  card.style.transform = "none";
+  // card.style.transform = "none";
   //popup
   title.style.transform = `translateZ(150px)`;
 });
